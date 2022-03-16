2de249ba8a4d762667c59e2d74d790960e9e133a
Ryland Taniguchi <ryland0000@gmail.com>
2022-03-16 09:29:09 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+/*-----------------------------------------------------------------
+Challenge: 02-addTwoNumbers
+Difficulty: Basic  
+Prompt:
+Write a function called addTwoNumbers that accepts two numeric arguments and returns the sum of those two numbers.
+If either argument is not a Number, return the value of NaN.
+Examples:
+addTwoNumbers(5, 10) //=> 15
+addTwoNumbers(10, -2) //=> 8
+addTwoNumbers(0, 0) //=> 0
+addTwoNumbers('Hello', 5) //=> NaN
+-----------------------------------------------------------------*/
+// Your solu
+</div>
+`;
